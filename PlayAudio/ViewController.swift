d017361c6edf7678aed0175209bdbf97454b8161
Sandy Gomez <sgomez1029@gmail.com>
2024-05-16 06:52:51 -0700

    Playing Audio in Storyboards

--- PlayAudio/ViewController.swift
+++ PlayAudio/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       loadAudioFile()
+    }
+    
+    func loadAudioFile() {
+        guard let audioFilePath = Bundle.main.path(forResource: "MeditationSong", ofType: "mp3") else {
+            return
+        }
+        let audioFileURL = URL(fileURLWithPath: audioFilePath)
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFileURL, fileTypeHint: nil)
+            audioPlayer.numberOfLoops = 0
+        } catch {
+            print ("AVAudioPlayer error = \(error)")
+        }
     }
 
-
+    @IBAction func stopButton(_ sender: UIButton) {
+        audioPlayer.stop()
+        loadAudioFile()
+    }
+    
+    @IBAction func playButton(_ sender: UIButton) {
+        audioPlayer.play()
+    }
+    
+    
+    @IBAction func pauseButton(_ sender: UIButton) {
+        audioPlayer.pause()
+    }
 }
 
